e9fe0e005be904df15fb4d1aad1a937bcf437287
Cote <majovardon@gmail.com>
2018-08-31 12:25:48 -0400

    Comenzando a hacer la validación

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const fs = require('fs');
 const Marked = require('marked');
 const fetch = require('node-fetch');
 const path = require('path');
-//const argv = require('yargs').argv;
+const argv = require('yargs')
+  .command('validate', 'Imprime la validación de los links en consola', {
+    validate: {
+      demand: true,
+      alias: 'val'
+    }
+  })
+  .help()
+  .argv;
 const colors = require('colors');
 
 // Función para extraer los links del archivo
@@ -42,30 +50,37 @@ const mdlinks = function markdownLinkExtractor(markdown) {
 // Busco dónde se está ejecutando el archivo (directorio)
 let dir = process.cwd();
 
+// Para dar la opción del validate, hay que rescatar el comando que pone el usuario
+let argv2 = process.argv;
+let val = argv[2];
+
+validateLink()
+
 // Lee el contenido del directorio
 fs.readdir(dir, (err, files) => {
-    if (err) throw err;
-    return files.forEach(file => {
-      // Sólo seleccionamos los archivos con la extensión .md
-      if (path.extname(file) === '.md') {
-        // Nos metemos en éstos archivos
-        fs.readFile(file, 'utf-8', (err, data) => {
-          if (err) throw err;
-          // Retorna un array de objetos
-          return mdlinks(data).forEach(element => {
-            fetch(`${element.href}`)
-              .then((answer) => {
-                let array = {
-                  link: answer.url,
-                  text: element.text,
-                  title: file,
-                  status: answer.status
-                }
-                console.log(array)
-              })
-              .catch(err => console.log(`El link ${element.href} está roto`.red))
-          })
+  if (err) throw err;
+  return files.forEach(file => {
+    // Sólo seleccionamos los archivos con la extensión .md
+    if (path.extname(file) === '.md') {
+      // Nos metemos en éstos archivos
+      fs.readFile(file, 'utf-8', (err, data) => {
+        if (err) throw err;
+        // Retorna un array de objetos
+        return mdlinks(data).forEach(element => {
+          fetch(`${element.href}`)
+            .then((answer) => {
+              let array = {
+                link: answer.url,
+                text: element.text,
+                title: file,
+                status: answer.status
+              }
+              console.log(array.link, array.text, array.title, array.status)
+            })
+            .catch(err => console.log(`El link ${element.href} está roto`.red))
         })
-      }
-    })
+      })
+    }
   })
+})
+
